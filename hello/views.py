31cdb1dc09b9913ae17ab778468409a5a056b043
davinbirdi <davin.birdi@hotmail.ca>
2020-05-13 14:06:46 -0700

    made changes to dev

--- hello/views.py
+++ hello/views.py
@@ -15,7 +15,7 @@ def index(request):
     
     times = int(os.environ.get('YELLOW'))
 
-    return HttpResponse('THIS IS A DEV BRANCH!!! WILL I BE PROMOTED!!??\n ' * times)
+    return HttpResponse("Let's propagate this change: dev -> stage -> prod\n" * times)
 
     # return HttpResponse('Hello from Python!')
     #return render(request, "index.html")
